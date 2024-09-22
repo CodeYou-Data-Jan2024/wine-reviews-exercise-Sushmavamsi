0ff6603fe044925e01a2008efa1fb1e99acc3327
sushm <sushmamurakonda@gmail.com>
2024-09-21 22:04:38 -0400

    code added

--- reviews.py
+++ reviews.py
@@ -1,2 +1,7 @@
-# add your code here
+import pandas as pd
 
+reviews=pd.read_csv('data/winemag-data-130k-v2.csv.zip',compression='zip')
+#print(reviews)
+summary=reviews.groupby('country').points.agg(['count',('points','mean')]).sort_values('count',ascending=False)
+#print(summary)
+summary.to_csv('data/reviews-per-country.csv')
