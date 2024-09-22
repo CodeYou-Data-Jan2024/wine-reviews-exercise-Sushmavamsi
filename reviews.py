afe7849c13bde55a6d238d8c7fbbf34f1ba65a88
sushm <sushmamurakonda@gmail.com>
2024-09-21 22:16:29 -0400

    final code

--- reviews.py
+++ reviews.py
@@ -3,5 +3,6 @@ import pandas as pd
 reviews=pd.read_csv('data/winemag-data-130k-v2.csv.zip',compression='zip')
 #print(reviews)
 summary=reviews.groupby('country').points.agg(['count',('points','mean')]).sort_values('count',ascending=False)
-#print(summary)
+summary['points']=summary.points.round(1)
+print(summary)
 summary.to_csv('data/reviews-per-country.csv')
